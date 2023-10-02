a5fe7d32d4cecbd46b08f27ec1c227307750d57f
kalyan <kalyanr444@gmail.com>
2023-10-02 21:46:56 +0530

    UC3committed

--- HashTablesD15/src/com/hashtables/Main.java
+++ HashTablesD15/src/com/hashtables/Main.java
@@ -1,21 +1,31 @@
 package com.hashtables;
 
 
-    public class Main {
-        public static void main(String[] args) {
-            String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+public class Main {
+    public static void main(String[] args) {
+        String phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
-            // Create a MyMap with 100 slots.
-            MyMap myMap = new MyMap(100);
+        // Create a MyMap with 100 slots.
+        MyMap myMap = new MyMap(100);
 
-            // Split the paragraph into words.
-            String[] words = paragraph.split(" ");
+        // Add the word "avoidable" to the MyMap with the value `true`.
+        myMap.put("avoidable", true);
 
-            // For each word, calculate the hashcode and use it to find the index in the MyMap LinkedList.
-            for (String word : words) {
-                int index = word.hashCode() % myMap.linkedLists.length;
+        // Split the phrase into words.
+        String[] words = phrase.split(" ");
 
+        // Create a new phrase to store the words that are not avoidable.
+        StringBuilder newPhrase = new StringBuilder();
 
+        // For each word in the original phrase, check if it is avoidable in the MyMap.
+        // If the word is not avoidable, add it to the new phrase.
+        for (String word : words) {
+            if (!myMap.containsKey(word)) {
+                newPhrase.append(word).append(" ");
             }
         }
+
+        // Print the new phrase.
+        System.out.println(newPhrase.toString());
     }
+}
